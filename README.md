7908d9632d481421227a93f2aabf3ba7522ae7af
Timothée Billiet Cadart <billiet.timothee@gmail.com>
2024-10-16 21:03:27 +0200

    Added `[POST]` /mountains endpoint

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ruby-rest-api
 💎 Learning Ruby! 💎
+
+# `[GET]` /mountains
+
+```bash
+curl -X GET "http://localhost:4567/mountains?name=mont-blanc"
+```
+
+# `[POST]` /mountains
+```bash
+curl -X POST "http://localhost:4567/mountains" -H "Content-Type: application/json" -d '{"name": "la-meije", "title": "La Meije", "altitude": 3984}'
+```
--- app.rb
+++ app.rb
@@ -30,3 +30,44 @@ get '/mountains' do
     { error: "Database error: #{e.message}" }.to_json
   end
 end
+
+post '/mountains' do
+  begin
+    body_contents = request.body.read
+
+    begin
+      data = JSON.parse(body_contents)
+    rescue JSON::ParserError => e
+      halt 400, { error: "JSON parsing error: #{e.message}" }.to_json
+    end
+
+    if (
+      data.is_a?(Hash) &&
+      data.key?('name') && data['name'].is_a?(String) &&
+      data.key?('title') && data['title'].is_a?(String) &&
+      data.key?('altitude') && data['altitude'].is_a?(Numeric) &&
+      (data.key?('description') ? data['description'].is_a?(String) : true)
+    )
+      sanitized_name = name.gsub(/[^a-z-]/, '')
+      sanitized_title = Sanitize.fragment(data['title'], Sanitize::Config::BASIC)
+      sanitized_description = Sanitize.fragment(data['description'], Sanitize::Config::BASIC)
+
+      response = DB.execute(
+        "INSERT INTO mountains (title, name, altitude, description) VALUES (?, ?, ?, ?)",
+        [
+          sanitized_title,
+          sanitized_name,
+          data['altitude'],
+          sanitized_description,
+        ]
+      )
+
+        puts response
+        status 201
+        { message: "Mountain created successfully" }.to_json
+    else
+      status 400
+      { error: "Invalid input" }.to_json
+    end
+  end
+end
